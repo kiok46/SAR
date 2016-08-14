564ffeece0fc9353b544cd99381ef6615913d283
kiok46 <kuldeepbb.grewal@gmail.com>
2016-08-14 12:55:36 +0530

    add description and update to 8

--- server.py
+++ server.py
@@ -17,6 +17,18 @@ def api_domain():
     return render_template('main.html')
 
 
+@app.route('/google_play/<package_name>')
+def api_google_data(package_name):
+    from oauth2client.client import OAuth2WebServerFlow
+    flow = OAuth2WebServerFlow(client_id='201223294714-5pjkgq831njk0sk2hhevckko6d1ee42o.apps.googleusercontent.com',
+                               client_secret='IcEqAGbiEtpXO8CHk5a2EArV',
+                               scope='https://www.googleapis.com/auth/androidpublisher',
+                               redirect_uri='http://localhost:5000')
+
+    auth_uri = flow.step1_get_authorize_url()
+    return auth_uri
+
+
 @app.route('/sentiment/<statement>')
 def api_sentiment_ext(statement):
     if statement == '':
@@ -63,8 +75,8 @@ def api_search_ext(search_name):
 
     api_head = 'https://data.42matters.com/api/v2.0/android/apps/' + \
                'search.json?q={}&include_developer=false&' + \
-               'include_desc=false&limit=5&access_token={}'
-    apikey = '4b0cff026e913e19d5b3a18ac5d8523ee07d8c2f '
+               'include_desc=false&limit=8&access_token={}'
+    apikey = '823aafff0eabb49e60edba8bc94ff153712c6104'
 
     common = Common()
     common.set_head(api_head)
@@ -73,23 +85,35 @@ def api_search_ext(search_name):
 
     api = common.get_api()
     rs = urlopen(api).read()
-
-    result_ = json.loads(rs)
+    # result = json.dumps(json.loads(rs))
+    # print result[0]
+    # for i in result['results']:
+    #    print i
+    # print result
     dict_ = {}
     package_name_ = []
     title_ = []
     icon_ = []
+    description_ = []
+
+    result_ = json.loads(rs)
+    out_file = open("search.json", "w")
+    json.dump(result_, out_file, indent=4)
+    out_file.close()
 
     for i in result_['results']:
         package_name_.append(i['package_name'].encode('utf-8'))
         title_.append(i['title'].encode('utf-8'))
         icon_.append(i['icon'].encode('utf-8'))
+        description_.append(i['description'].encode('utf-8'))
+    # print result_['results']
 
     dict_["package_name"] = package_name_
     dict_["title"] = title_
     dict_["icon"] = icon_
+    dict_["description"] = description_
 
-    print dict_
+    # d = json.dumps(json.loads(dict_))
     return '{}'.format(json.dumps(dict_))
 
 
